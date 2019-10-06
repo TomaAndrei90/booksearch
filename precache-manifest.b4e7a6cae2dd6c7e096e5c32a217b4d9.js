bbaf252104dc38b1e3ce3fdc69a01e4fa394d496
Andrei Toma <toma.andrei90@yahoo.com>
2019-10-06 22:18:48 +0300

    Updates

---
+++ precache-manifest.b4e7a6cae2dd6c7e096e5c32a217b4d9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64fc90816662a4e1ba6bb5bb45a45d1",
+    "url": "/booksearch/index.html"
+  },
+  {
+    "revision": "ef353f82028295b69127",
+    "url": "/booksearch/static/css/2.d5671812.chunk.css"
+  },
+  {
+    "revision": "1866d2494da6f6580535",
+    "url": "/booksearch/static/css/main.67b3be07.chunk.css"
+  },
+  {
+    "revision": "ef353f82028295b69127",
+    "url": "/booksearch/static/js/2.a42c1c68.chunk.js"
+  },
+  {
+    "revision": "1866d2494da6f6580535",
+    "url": "/booksearch/static/js/main.cfba7b6d.chunk.js"
+  },
+  {
+    "revision": "315441a7799a03b3e258",
+    "url": "/booksearch/static/js/runtime-main.5d1554b7.js"
+  },
+  {
+    "revision": "9382b83f444dc129e42bc335f90e3513",
+    "url": "/booksearch/static/media/book.9382b83f.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/booksearch/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/booksearch/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/booksearch/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/booksearch/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/booksearch/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/booksearch/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/booksearch/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/booksearch/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/booksearch/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/booksearch/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/booksearch/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/booksearch/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/booksearch/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/booksearch/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/booksearch/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/booksearch/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
